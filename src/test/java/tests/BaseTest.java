ca3a95392ee17bfa40a619fca90ef0df8f905fa2
Bhanupriya C N <bhanupriyacn@Bhanupriyas-Laptop.local>
2025-06-08 10:47:56 -0500

    Updated BaseTest with Edge support and improvements

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -18,23 +20,35 @@ public class BaseTest {
     @BeforeMethod(alwaysRun = true)
     public void setUp(@Optional("chrome") String browser) {
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new"); // new headless mode for Chrome 109+
-            options.addArguments("--window-size=1920,1080");
-            options.addArguments("--disable-gpu"); // optional on Mac/Linux
-            options.addArguments("--remote-allow-origins=*"); // Chrome 111+ fix
-            driver = new ChromeDriver(options);
+        System.out.println("Launching browser: " + browser);
 
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
-            options.addArguments("--width=1920");
-            options.addArguments("--height=1080");
-            driver = new FirefoxDriver(options);
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new"); // headless Chrome 109+
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new"); // headless Edge (Chromium-based)
+                edgeOptions.addArguments("--window-size=1920,1080");
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
 
         // Common settings
@@ -48,7 +62,6 @@ public class BaseTest {
             driver.quit();
         }
     }
-
 
     public WebDriver getDriver() {
         return driver;
